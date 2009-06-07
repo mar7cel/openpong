7464a5b06752db92eb8b9478289c242a17842052
maik.loehn <maik.loehn@gmx.de>
2009-06-07 15:33:38 +0000

    Neue Sound Klasse

---
+++ sound.hpp
@@ -0,0 +1,18 @@
+#ifndef SOUND_HPP_INCLUDED
+#define SOUND_HPP_INCLUDED
+
+#include "framework.hpp"
+
+class CSound
+{
+    public  :
+                CSound();
+                ~CSound();
+                void LoadSound(const string sFilename); //Lädt den Angegeben Sound
+                void Play();                            //Spielt den geladen Sound
+
+    private :
+                Mix_Music *music;          //Datentyp für die Musik
+};
+
+#endif // SOUND_HPP_INCLUDED
